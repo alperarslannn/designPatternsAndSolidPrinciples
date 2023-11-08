bb545bc3c4a6f6702dc476798b7f4640f1e31ee7
alperarslannn <alper.arslan663@gmail.com>
2023-11-08 10:34:55 +0300

    Section 16-17 - Facade and Flyweight added.

---
+++ Section 16 - Facade/udemy-design-patterns-handson/src/com/coffeepoweredcrew/facade/email/EmailFacade.java
@@ -0,0 +1,9 @@
+package com.coffeepoweredcrew.facade.email;
+
+import com.coffeepoweredcrew.facade.Order;
+import com.coffeepoweredcrew.facade.email.Template.TemplateType;
+
+//Facade provides simple methods for client to use
+public class EmailFacade {
+	
+}
